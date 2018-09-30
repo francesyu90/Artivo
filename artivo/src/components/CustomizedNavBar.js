af1852584ec517618c8d7e3d522a326389480e36
Frances Yu <francesyu90@yahoo.com>
2018-09-30 11:03:39 -0700

    feat: Added customized nav bar

---
+++ artivo/src/components/CustomizedNavBar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class CustomizedNavBar extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar position="static" className="c-nav-bar">
+                    <Toolbar>
+                        <Typography variant="title" color="inherit">
+                            Artivo
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+export default CustomizedNavBar;
